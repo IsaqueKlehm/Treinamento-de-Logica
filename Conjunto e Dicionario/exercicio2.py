3bdc6a2c246223bea4c4cec098857713e03e7ff4
Isaque Klehm <isaqueklehm@gmail.com>
2026-09-17 17:26:09 -0300

    Update: Exercício 1, 2 e 3 com enunciados agora

--- Conjunto e Dicionario/exercicio2.py
+++ Conjunto e Dicionario/exercicio2.py
@@ -1,3 +1,6 @@
+# Clara é editora de uma revista e deseja comparar dois artigos para identificar quais palavras aparecem em ambos. 
+# Sua tarefa é criar um programa que receba dois textos e exiba o conjunto de palavras comuns entre eles.
+
 texto1 = set(input("Digite a frase: ").lower().split(" "))
 texto2 = set(input("Digite outra frase: ").lower().split(" "))
 
--- Conjunto e Dicionario/exercicio3.py
+++ Conjunto e Dicionario/exercicio3.py
@@ -1,3 +1,14 @@
+# Laura e Ana resolveram fazer compras juntas, mas criaram duas listas diferentes. 
+# Elas querem um programa que mostre:
+
+# Quais itens apareceram nas duas listas
+
+# Quais foram exclusivos de Laura
+
+# Quais foram exclusivos da Ana
+
+# Escreva um programa que solicite as listas e mostre os resultados dessas comparações.
+
 lista1 = set(input("Digite a lista de compra da pessoa 1: ").lower().split(", "))
 lista2 = set(input("Digite a lista de compra da pessoa 2: ").lower().split(", "))
 
