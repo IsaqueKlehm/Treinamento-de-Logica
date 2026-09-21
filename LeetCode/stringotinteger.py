9ad8022a0beded03261f12c8f88abca7bff1c41b
Isaque Klehm <isaqueklehm@gmail.com>
2026-09-21 20:26:53 -0300

    Update: exercicio do leetcode

---
+++ LeetCode/stringotinteger.py
@@ -0,0 +1,25 @@
+numero = input("Digite a sua senha: ")
+lista = []
+print()
+for i in numero:
+    # Verifica se é negativo
+    if i == "-":
+        print(i, end="")
+        lista.append(i)
+    try:
+        if int(i):
+            pass
+    except:
+        if i == "-":
+            pass
+        else:
+            break
+    try:
+        # Se tem numero nas letra
+        if int(i):
+            print(i, end="")
+            lista.append(i)
+    except:
+        pass
+if len(lista) == 0:
+    print(0)
